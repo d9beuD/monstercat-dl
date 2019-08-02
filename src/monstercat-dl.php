8cd544f34453851fc87f48ec2fb117bd07794f1c
d9beuD <53569029+d9beuD@users.noreply.github.com>
2019-08-02 10:16:05 +0200

    Move monstercat-dl.php to /src directory

--- src/monstercat-dl.php
+++ src/monstercat-dl.php
@@ -14,6 +14,19 @@ function usage()
     'Usage: ' . APP_NAME . ' [RELEASE_ID]...' . PHP_EOL;
 }
 
+function parseArgs($argv){
+    array_shift($argv); $o = array();
+    foreach ($argv as $a){
+        if (substr($a,0,2) == '--'){ $eq = strpos($a,'=');
+            if ($eq !== false){ $o[substr($a,2,$eq-2)] = substr($a,$eq+1); }
+            else { $k = substr($a,2); if (!isset($o[$k])){ $o[$k] = true; } } }
+        else if (substr($a,0,1) == '-'){
+            if (substr($a,2,1) == '='){ $o[substr($a,1,1)] = substr($a,3); }
+            else { foreach (str_split(substr($a,1)) as $k){ if (!isset($o[$k])){ $o[$k] = true; } } } }
+        else { $o[] = $a; } }
+    return $o;
+}
+
 /**
  * Get release information
  * @param  string $key The release key on Monstercat website
@@ -50,8 +63,6 @@ if ($argc > 1) {
             $albums[] = $value;
         }
     }
-
-
 
     foreach ($albums as $key => $albumKey) {
         $release = getRelease($albumKey);
