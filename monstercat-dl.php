d81220dc17ada742823e3047de8a0d1d6c31ba78
Vincent Bathelier <53569029+d9beuD@users.noreply.github.com>
2022-01-09 19:11:28 +0100

    Create new start point for console app

---
+++ monstercat-dl.php
@@ -0,0 +1,10 @@
+<?php
+
+use MonstercatDl\CLI\DownloadCommand;
+use Symfony\Component\Console\Application;
+
+require_once __DIR__ . '/bootstrap.php';
+
+$app = new Application('mobstercat-dl', '2.0.0');
+
+$app->run();
